65557fa7e5d9f99f67f8bd9a8ccc82b31fb82d08
Nur Muhammad Sholikhun <tanyaikhun@gmail.com>
2018-03-14 15:11:16 +0700

    Delete function CounTotalMotor and add recountMotor, countJumlahMotor, kilometerMotor

--- functions/index.js
+++ functions/index.js
@@ -18,6 +18,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+const rp = require('request-promise');
+const promisePool = require('es6-promise-pool');
+const PromisePool = promisePool.PromisePool;
+const secureCompare = require('secure-compare');
+// Maximum concurrent account deletions.
+const MAX_CONCURRENT = 3;
 
 // Keeps track of the length of the 'likes' child list in a separate property.
 exports.countJumlahMotor = functions.database.ref('motors/{uid}/{motorid}').onWrite((event) => {
@@ -73,3 +79,58 @@ exports.recountMotor = functions.database.ref('users/{uid}/totalMotor').onWrite(
   }
   return null;
 });
+
+exports.kilometerMotor = functions.https.onRequest((req, res) => {
+  const key = req.query.key;
+
+  // Exit if the keys don't match
+  if (!secureCompare(key, functions.config().cron.key)) {
+    console.log('The key provided in the request does not match the key set in the environment. Check that', key,
+      'matches the cron.key attribute in `firebase env:get`');
+    res.status(403).send('Security key does not match. Make sure your "key" URL query parameter matches the ' +
+      'cron.key environment variable.');
+    return null;
+  }
+  var query = admin.database().ref("users").orderByKey();
+  query.once("value")
+  .then(snapshot => {
+    snapshot.forEach(childSnapshot => {
+      // key will be "ada" the first time and "alan" the second time
+      var key = childSnapshot.key;
+      // childData will be the actual contents of the child
+      var childData = childSnapshot.val();
+      
+      getMotor(key);
+      
+    }); 
+    return null;
+  }).catch();
+    
+  return res.send('finish');
+  // Fetch all user details.
+  // return getUsers();
+});
+
+function getMotor(key){
+  let motor, motors = [];
+      const oldItemsQuery = admin.database().ref('/motors/'+key).orderByKey();
+      oldItemsQuery.once('value').then(snapshot => {
+        // create a map with all children that need to be removed
+        
+        snapshot.forEach((childSnapshot) => {
+          motor = childSnapshot.val();
+          console.log("Data motor : "+motor.idmotor);
+          motors.push(motor);
+          
+          var kilometer = 2*(motor.km_ratarata) + motor.km_now;
+        console.log("Km motor "+kilometer);
+        const query = admin.database().ref('/motors/'+key+'/'+motor.idmotor).update({km_now: kilometer});
+        });
+        // console.log(motors.length + " motor retrieved");
+        // console.log("ID Motor: "+motor.idmotor);
+        
+
+        return true;
+        
+  }).catch();
+}
