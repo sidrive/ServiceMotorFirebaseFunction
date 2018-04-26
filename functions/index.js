596921461c6139ba665fd0abc32c04913d432974
Nur Muhammad Sholikhun <tanyaikhun@gmail.com>
2018-04-26 16:22:58 +0700

    Add functions notifBayarPajak and change sendNotif

--- functions/index.js
+++ functions/index.js
@@ -180,13 +180,13 @@ function getDataMotor(childData,key){
           console.log("Data motor : "+motor.idmotor);
           console.log("Data User :"+childData.uid);
           console.log("Data U :"+motor.km_NextService+"-"+motor.km_now);
-          var ok = motor.km_NextService-motor.km_now;
-          console.log("Data s :"+ok);
+          var selisihKM = motor.km_NextService-motor.km_now;
+          console.log("Data s :"+selisihKM);
           motors.push(motor);
           // sendNotif(motor,childData);
-          if(ok <=700){
-            
-            sendNotif(motor,childData);
+          if(selisihKM <=700){
+            var status = "limitKm"
+            sendNotif(motor,childData,status);
           }
         //   var kilometer = 2*(motor.km_ratarata) + motor.km_now;
         // console.log("Km motor "+kilometer);
@@ -200,7 +200,7 @@ function getDataMotor(childData,key){
   }).catch();
 }
 
-function sendNotif(motor,childData) {
+function sendNotif(motor,childData,status) {
 
     const userID = childData.uid;
     // const orderID = order.oid;
@@ -214,8 +214,7 @@ function sendNotif(motor,childData) {
 
       return Promise.all([getUserTokensPromise, getUserProfilePromise]).then(results => {
       const tokensSnapshot = results[0];
-      const user = results[1];
-      const titleNotifications = 'Peringatan SERVICE !!';
+      const user = results[1]; 
 
     // Check if there are any device tokens.
     if (!tokensSnapshot.hasChildren()) {
@@ -225,26 +224,84 @@ function sendNotif(motor,childData) {
     console.log('User data', user);
 
     // Notification details.
-    const payload = {
-      data: {  
-            orderid: motor.idmotor,            
-        },
-      notification: {
-        title: titleNotifications,
-        body: `${user.displayName} Motor Anda ${motor.merk} ${motor.plat} hampir memasuki masa service,` || user.photoURL,
-        sound: `default`,
-        icon: `ic_add`,
-        color: 'red',
-        priority: `high`,
-        vibrate: `[0,250,200,250]`,
-        timeToLive: `60 * 60 * 24`
-      }
-    };
-    
-    // Listing all tokens.
-    const tokens = Object.keys(tokensSnapshot.val());
+    if (status === "limitPajak"){
+      const titleNotifications = 'Jangan Lupa PAJAK !!';
+      const bodyMessage = `${user.displayName} Jangan Lupa Bayar Pajak Motor Anda ${motor.merk} ${motor.plat},` || user.photoURL;  
+      const payload = {
+        data: {  
+              orderid: motor.idmotor,            
+          },
+        notification: {
+          title: titleNotifications,
+          body: bodyMessage,
+          sound: `default`,
+          icon: `ic_add`,
+          color: 'red',
+          priority: `high`,
+          vibrate: `[0,250,200,250]`,
+          timeToLive: `60 * 60 * 24`
+        }
+      };
+  
+      
+      // Listing all tokens.
+      const tokens = Object.keys(tokensSnapshot.val());
+  
+      send(tokens,payload);
+    }
+    if (status === "limitKm"){
+      const titleNotifications = 'Jangan Lupa SERVICE !!';
+      const bodyMessage = `${user.displayName} Jangan Lupa Service Motor Anda ${motor.merk} ${motor.plat},` || user.photoURL;
+      
+      const payload = {
+        data: {  
+              orderid: motor.idmotor,            
+          },
+        notification: {
+          title: titleNotifications,
+          body: bodyMessage,
+          sound: `default`,
+          icon: `ic_add`,
+          color: 'red',
+          priority: `high`,
+          vibrate: `[0,250,200,250]`,
+          timeToLive: `60 * 60 * 24`
+        }
+      };
+  
+      
+      // Listing all tokens.
+      const tokens = Object.keys(tokensSnapshot.val());
+  
+      send(tokens,payload);
+    }
+    if (status === "limitTglservice"){
+      const titleNotifications = 'Jangan Lupa SERVICE !!';
+      const bodyMessage = `${user.displayName} Jangan Lupa Service Motor Anda ${motor.merk} ${motor.plat},` || user.photoURL;
 
-    send(tokens,payload);
+      const payload = {
+        data: {  
+              orderid: motor.idmotor,            
+          },
+        notification: {
+          title: titleNotifications,
+          body: bodyMessage,
+          sound: `default`,
+          icon: `ic_add`,
+          color: 'red',
+          priority: `high`,
+          vibrate: `[0,250,200,250]`,
+          timeToLive: `60 * 60 * 24`
+        }
+      };
+  
+      
+      // Listing all tokens.
+      const tokens = Object.keys(tokensSnapshot.val());
+  
+      send(tokens,payload);
+    }    
+    
     return true;
   });  
     
@@ -279,4 +336,74 @@ function sendNotif(motor,childData) {
       return Promise.all(tokensToRemove);
     });
   }
+}
+
+
+exports.notifBayarPajak = functions.https.onRequest((req, res) => {
+  const key = req.query.key;
+
+  // Exit if the keys don't match
+  if (!secureCompare(key, functions.config().cron.key)) {
+    console.log('The key provided in the request does not match the key set in the environment. Check that', key,
+      'matches the cron.key attribute in `firebase env:get`');
+    res.status(403).send('Security key does not match. Make sure your "key" URL query parameter matches the ' +
+      'cron.key environment variable.');
+    return null;
+  }
+  var query = admin.database().ref("users").orderByKey();
+  query.once("value")
+  .then(snapshot => {
+    snapshot.forEach(childSnapshot => {
+      // key will be "ada" the first time and "alan" the second time
+      var key = childSnapshot.key;
+      // childData will be the actual contents of the child
+      var childData = childSnapshot.val();
+      
+      getTglPajak(childData,key);
+      
+    }); 
+    return null;
+  }).catch();
+    
+  return res.send('finish');
+  // Fetch all user details.
+  // return getUsers();
+});
+
+function getTglPajak(childData,key){
+  let motor, motors = [];
+      const oldItemsQuery = admin.database().ref('/motors/'+key).orderByKey();
+      oldItemsQuery.once('value').then(snapshot => {
+        // create a map with all children that need to be removed
+        
+        snapshot.forEach((childSnapshot) => {
+          motor = childSnapshot.val();
+          console.log("Data motor : "+motor.idmotor);
+          console.log("Data User :"+childData.uid);
+
+          var today = new Date().getTime();
+          var difference = motor.tahun_pajak - today;
+          var daysDifference = Math.floor(difference/1000/60/60/24);
+          difference -= daysDifference*1000*60*60*24
+
+          var d = new Date(today);
+          var e = new Date(motor.tahun_pajak);
+          
+          console.log("Today : "+d);
+          console.log("Tgl Pajak : "+e);
+          console.log("Data tanggal1 :"+daysDifference);
+          motors.push(motor);
+          
+          if(daysDifference <=15){
+            var status = "limitPajak";
+            sendNotif(motor,childData,status);
+          }
+        
+        });
+        // console.log(motors.length + " motor retrieved");
+        // console.log("ID Motor: "+motor.idmotor);     
+
+        return true;
+        
+  }).catch();
 }
