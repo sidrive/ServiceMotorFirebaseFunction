0fc620d53209693d55fb84b38284997e9667517e
Nur Muhammad Sholikhun <tanyaikhun@gmail.com>
2018-04-30 11:26:45 +0700

    Update firebase function to V1.0 ~Change dependencies functions counJumlahMotor & recountMotor

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,8 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+// admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 const rp = require('request-promise');
 const promisePool = require('es6-promise-pool');
 const PromisePool = promisePool.PromisePool;
@@ -26,15 +27,15 @@ const secureCompare = require('secure-compare');
 const MAX_CONCURRENT = 3;
 
 // Keeps track of the length of the 'likes' child list in a separate property.
-exports.countJumlahMotor = functions.database.ref('motors/{uid}/{motorid}').onWrite((event) => {
-  const collectionRef = event.data.adminRef.root;
-  const uid    = event.params.uid;
-  const countRef = collectionRef.child('users/'+uid+'/totalMotor');
+exports.countJumlahMotor = functions.database.ref('motors/{uid}/{motorid}').onWrite((change, context) => {
+  // const collectionRef = change.data.adminRef.root;
+  const uid    = context.params.uid;
+  const countRef = admin.database().ref('users/'+uid+'/totalMotor');
 
   let increment;
-  if (event.data.exists() && !event.data.previous.exists()) {
+  if (change.after.exists() && !change.before.exists()) {
     increment = 1;
-  } else if (!event.data.exists() && event.data.previous.exists()) {
+  } else if (!change.after.exists() && change.before.exists()) {
     increment = -1;
   } else {
     return null;
@@ -63,22 +64,35 @@ exports.countJumlahMotor = functions.database.ref('motors/{uid}/{motorid}').onWr
   
 // });
 
-exports.recountMotor = functions.database.ref('users/{uid}/totalMotor').onWrite((event) => {
-  if (event.data.exists()) {
-    const collectionRef = event.data.adminRef.root;
-  const uid    = event.params.uid;
-  const countRef = collectionRef.child('users/'+uid+'/totalMotor');
-  const cRef = collectionRef.child('motors/'+uid);
+// exports.recountMotor = functions.database.ref('users/{uid}/totalMotor').onWrite((change, context) => {
+//   if (change.data.exists()) {
+//     const collectionRef = change.data.adminRef.root;
+//   const uid    = change.params.uid;
+//   const countRef = collectionRef.child('users/'+uid+'/totalMotor');
+//   const cRef = collectionRef.child('motors/'+uid);
     
 
-    // Return the promise from counterRef.set() so our function
-    // waits for this async event to complete before it exits.
-    return cRef.once('value')
-      .then((messagesData) => countRef.set(messagesData.numChildren())).then(() =>{ return console.log('Total update');});
+//     // Return the promise from counterRef.set() so our function
+//     // waits for this async event to complete before it exits.
+//     return cRef.once('value')
+//       .then((messagesData) => countRef.set(messagesData.numChildren())).then(() =>{ return console.log('Total update');});
       
-  }
-  return null;
+//   }
+//   return null;
+// });
+
+exports.recountMotor = functions.database.ref('users/{uid}/totalMotor').onWrite((change, context) => {
+    
+  const uid    = context.params.uid;
+  const countRef = admin.database().ref('users/'+uid+'/totalMotor');
+  const cRef = admin.database().ref('motors/'+uid);
+
+  // Return the promise from counterRef.set() so our function
+  // waits for this async event to complete before it exits.
+  return cRef.once('value')
+  .then((messagesData) => countRef.set(messagesData.numChildren())).then(() =>{ return console.log('Total Motor update');});
 });
+
 
 exports.kilometerMotor = functions.https.onRequest((req, res) => {
   const key = req.query.key;
